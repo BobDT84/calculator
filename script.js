b66ae89eb6778f30a34496be4c2ff0b151ed1a47
Bob <bobdt84@gmail.com>
2022-03-09 20:20:37 -0600

    Setting outline and plan of functions and testing how to parse button presses

--- script.js
+++ script.js
@@ -2,6 +2,33 @@ function nameMe(event){
     console.log(this.id);
 }
 
+function deleteInput(){
+    input[current].pop();
+}
+
+function clearInput(){
+    input[current] = [];
+}
+
+function getPower(){
+    //is this going to need to be asynchronus
+    //to wait for the next button press?
+}
+
+function parentheses(){
+    //will need to determine is opening
+    //or closing the parentheses
+}
+
+function getResult(){
+    //parse the current input
+    //determine the proper calculation
+    //display current input to oupt
+    //display result of the calculation
+    //clear current input
+    clearInput();
+}
+
 let buttons = document.getElementsByTagName('button');
 
 for(let button of buttons){
@@ -10,3 +37,16 @@ for(let button of buttons){
 
 const input = {current:[], };
 const output = {input:[], result:0, };
+const buttonValues = {
+    divide: '/',
+    delete: deleteInput(),
+    clear: clearInput(),
+    multiply: '*',
+    square: '<sup>2</sup>',
+    squareRoot: '√',
+    subtract: '-',
+    power: getPower(),
+    parentheses: parentheses(),
+    add: '+',
+    equal: getResult(),
+}
