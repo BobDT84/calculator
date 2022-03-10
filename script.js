765091c7511e7cb06c1a1a300eb94ff14514601f
Bob <bobdt84@gmail.com>
2022-03-09 22:20:39 -0600

    Input can be updated and displayed, but the funtions in the buttonAction object literal are showing as undefined.

--- script.js
+++ script.js
@@ -1,39 +1,15 @@
 function nameMe(event){
     console.log(this.id);
-    console.log(buttonValues[this.id]);
-    if(this.id in input.buttonValues){
-        input.buttonValues[this.id];
-    } else {
-        input.current.push(this.id);
+    const buttonValue = input.buttonValue[this.id];
+    if(this.id in input.buttonAction){
+        input.buttonAction[this.id];
+    } else if(this.id in input.buttonValue){
+        input.pushInput(buttonValue);
     }
-    console.log(input.current);
-    displayInput();
 }
 
 
-function getPower(){
-    //is this going to need to be asynchronus
-    //to wait for the next button press?
-}
 
-function parentheses(){
-    //will need to determine is opening
-    //or closing the parentheses
-}
-
-function getResult(){
-    //parse the current input
-    //determine the proper calculation
-    //display current input to oupt
-    //display result of the calculation
-    //clear current input
-    clearInput();
-}
-
-function displayInput(){
-    let display = document.querySelector('#input');
-    display.innerText = input['current'].join('');
-}
 
 let buttons = document.getElementsByTagName('button');
 
@@ -41,31 +17,71 @@ for(let button of buttons){
     button.addEventListener('click', nameMe);
 }
 
-const input = {
+class Input {
+    constructor(){
+        this.input = [];
+        this.buttonAction = {
+            delete: this.deleteInput,
+            clear: this.clearInput(),
+            power: this.getPower(),
+            parentheses: this.parentheses(),
+            equal: this.getResult(),
+        };
+        this.buttonValue = {
+            divide: '/',
+            multiply: '*',
+            square: "^2 ",
+            squareRoot: '√',
+            subtract: '-',
+            add: '+',
+            0: '0',
+            1: '1',
+            2: '2',
+            3: '3',
+            4: '4',
+            5: '5',
+            6: '6',
+            7: '7',
+            8: '8',
+            9: '9',
+            decimal: '.',
+        };
+    }
+    pushInput(string){
+        this.input.push(string);
+        this.displayInput();
+    }
+    displayInput(){
+        let display = document.querySelector('#input');
+        display.innerText = this.input.join('');
+    }
     deleteInput(){
-        this.current.pop();
-        displayInput();
-    },
+        this.input.pop();
+        this.displayInput();
+    }
     clearInput(){
-        this.current = [];
-        displayInput();
-    },
-    buttonValues: {
-        divide: '/',
-        delete: deleteInput(),
-        clear: clearInput(),
-        multiply: '*',
-        square: "^2 ",
-        squareRoot: '√',
-        subtract: '-',
-        power: getPower(),
-        parentheses: parentheses(),
-        add: '+',
-        equal: getResult(),
-    },
-    current:[], 
-};
+        this.input = [];
+        this.displayInput();
+    }
+    getPower(){
+        //is this going to need to be asynchronus
+        //to wait for the next button press?
+    }
+    parentheses(){
+        //will need to determine is opening
+        //or closing the parentheses
+    }
+    getResult(){
+        //parse the current input
+        //determine the proper calculation
+        //display current input to oupt
+        //display result of the calculation
+        //clear current input
+        this.clearInput();
+    }
+}
 
+const input = new Input();
 const output = {input:[], result:0, };
 /*
 const buttonValues = {
